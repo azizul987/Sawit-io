35d9698e4fae9c478a4a88e6795e7b22216243b5
azizul987 <wawanajah954@gmail.com>
2026-07-22 20:57:41 +0700

    Setup folder Source dan ress untuk data template

---
+++ Logic/Data Templete/Data/Source/Skill.gd
@@ -0,0 +1,7 @@
+class_name skill
+extends  Resource
+
+@export var id:String
+@export var skill_name:String
+@export var max_level:int
+@export var  tree_position:Vector2
---
+++ Logic/Data Templete/Data/Source/SkillObj.gd
@@ -0,0 +1,5 @@
+class_name skill_obj
+
+extends  Resource
+
+@export var skills:Array[skill]=[]
