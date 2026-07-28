b2938e36e9fdf3e644f3a6f794ac3d7818592689
azizul987 <wawanajah954@gmail.com>
2026-07-28 21:26:59 +0700

    add random in event click tween

--- Logic/skrip/Click.gd
+++ Logic/skrip/Click.gd
@@ -110,8 +110,13 @@ func show_plus_effect(jumlah_poin: float) -> void:
 
 	label.text = "+%.1f" % jumlah_poin
 	label.z_index = 100
-	label.modulate = Color.WHITE
-
+	label.modulate = Color.from_hsv(
+		randf_range(0.22, 0.45), # hijau kekuningan sampai hijau kebiruan
+		randf_range(0.45, 1.0),  # kepekatan warna
+		randf_range(0.55, 1.0),  # terang-gelap
+		1.0
+	)
+	label.rotation = deg_to_rad(randf_range(-15.0, 15.0))
 	label.add_theme_font_override("font", FONT_KOSTUM)
 	label.add_theme_font_size_override("font_size", randi_range(20,50))
 
@@ -121,22 +126,35 @@ func show_plus_effect(jumlah_poin: float) -> void:
 	get_tree().current_scene.add_child(label)
 
 	label.global_position = get_global_mouse_position() + Vector2(randf_range(-15, 15), randf_range(-10, 5)) * z
+	var arah_gerak := Vector2(
+	randf_range(-35.0, 35.0),
+	randf_range(-80.0, -40.0)
+) * z
+
+	var durasi: float = randf_range(0.4, 0.9)
 
 	var tween := create_tween()
 	tween.set_parallel(true)
 
 	tween.tween_property(
-		label,
-		"global_position",
-		label.global_position + Vector2(0, -50 * z),
-		0.6
-	)
+	label,
+	"global_position",
+	label.global_position + arah_gerak,
+	durasi
+)
 
 	tween.tween_property(
 		label,
 		"modulate:a",
 		0.0,
-		0.6
+		durasi
+	)
+
+	tween.tween_property(
+		label,
+		"scale",
+		label.scale * randf_range(1.1, 1.5),
+		durasi
 	)
 
 	await tween.finished
