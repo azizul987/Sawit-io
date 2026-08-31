8ab205974fe00ad1054059ad34343309386916e0
azizul987 <wawanajah954@gmail.com>
2026-08-31 15:56:23 +0700

    final edit for ress will, skill and update

--- Logic/Data Templete/Data/Source/MissionDatabase.gd
+++ Logic/Data Templete/Data/Source/MissionDatabase.gd
@@ -3,9 +3,10 @@ extends Resource
 
 @export var missions: Array[Mission] = []
 
-func  get_mision(idx:int):
-	if idx<len(missions): return missions[idx]
-	return missions[0]
+func get_mision(idx: int) -> Mission:
+	if idx >= 0 and idx < missions.size():
+		return missions[idx]
+	return null
 func get_mision_type(idx:int):
 	if idx<len(missions): return missions[idx].requirement_type
 	return 0
